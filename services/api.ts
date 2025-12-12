1fed92295618b39b66aca35b31b5425646f745c2
Jay Codes <josephjchuchu@gmail.com>
2025-12-12 22:43:13 +0300

    better decoupling

--- services/api.ts
+++ services/api.ts
@@ -4,7 +4,7 @@ import {
   PageableResponse, ActivationRequest
 } from '../types';
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:7090';
+const BASE_URL = import.meta.env.VITE_BASE_URL ;
 
 const getHeaders = () => {
   const token = localStorage.getItem('token');
